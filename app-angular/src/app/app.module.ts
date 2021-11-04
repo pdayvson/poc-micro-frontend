68ec0b8dc8fc4de77d49216cb8a001366e11dc03
P. Dayvson <pdayvson@hotmail.com>
2021-11-04 02:42:17 -0300

    first commit

---
+++ app-angular/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { APP_BASE_HREF } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  // providers: [{provide: APP_BASE_HREF, useValue: '/angular'}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
